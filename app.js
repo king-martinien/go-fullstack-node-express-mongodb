449048d6656be14fde46ee3ee4723645b283e4d3
King-Martinien <martinienfokoue@yahoo.com>
2021-12-24 18:06:36 +0100

    modification et suppression des données dans la base de donnée

--- app.js
+++ app.js
@@ -39,7 +39,21 @@ app.post("/api/stuff", (req, res, next) => {
     .then(() =>
       res.status(201).json({ message: "Objet enregistré avec succès!" })
     )
+    .catch((error) => res.status(400).json({ error: error }));
+});
+
+app.put("/api/stuff/:id", (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Objet modifié !" }))
     .catch((error) => res.status(404).json({ error: error }));
+});
+
+app.delete("/api/stuff/:id", (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() =>
+      res.status(200).json({ message: "Objet supprimé avec succès !" })
+    )
+    .catch((error) => res.status(400).json({ error: error }));
 });
 
 app.get("/api/stuff/:id", (req, res, next) => {
@@ -54,7 +68,7 @@ app.get("/api/stuff", (req, res, next) => {
       res.status(200).json(things);
     })
     .catch((error) => {
-      res.status(404).json(error);
+      res.status(400).json(error);
     });
 });
 
