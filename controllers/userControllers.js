24384e52b4c478db756e0328ca3ea8b10513e77d
King-Martinien <martinienfokoue@yahoo.com>
2021-12-27 04:13:34 +0100

    verification de l'existaance d'un utilisateur avec la methode login

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -18,4 +18,23 @@ exports.signup = (req, res, next) => {
     .catch((error) => res.status(500).json({ error: error }));
 };
 
-exports.login = (req, res, next) => {};
+exports.login = (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({ error: "Utiliateur non crée !" });
+      }
+
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res.status(401).json({ error: "Mot de passe incorrect !" });
+          }
+
+          res.status(200).json({ userId: user._id, token: "TOKEN" });
+        })
+        .catch((error) => res.status(500).json({ error: error }));
+    })
+    .catch((error) => res.status(500).json({ error: error }));
+};
