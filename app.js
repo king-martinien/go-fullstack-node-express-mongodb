d85434218c8d7d7feb0e7a6e489ef3bf7af303ee
King-Martinien <martinienfokoue@yahoo.com>
2021-12-27 03:47:34 +0100

    creation de la router et du controller pour un utilisateur; implémentation de la methode signup pour créer un utilisateur

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const stuffRoutes = require("./routes/stuffRoutes");
-
+const userRoutes = require("./routes/userRouters");
 
 mongoose
   .connect(
@@ -32,5 +32,6 @@ app.use((req, res, next) => {
 });
 
 app.use("/api/stuff", stuffRoutes);
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
